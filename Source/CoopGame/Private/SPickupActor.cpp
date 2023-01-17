70d0bc5980382722978d263993cbf1388d418740
witmaos6 <geonhee518@naver.com>
2023-01-17 21:04:38 +0900

    Feat PowerupHealth code

--- Source/CoopGame/Private/SPickupActor.cpp
+++ Source/CoopGame/Private/SPickupActor.cpp
@@ -20,7 +20,7 @@ ASPickupActor::ASPickupActor()
 	DecalComp->DecalSize = FVector(64, 75, 75);
 	DecalComp->SetupAttachment(RootComponent);
 
-
+	CooldownDuration = 10.0f;
 }
 
 // Called when the game starts or when spawned
